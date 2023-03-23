31b5882c517ea9150c3002bdd66d0260282e37c5
JuanLu <juansvrs0321@gmail.com>
2023-03-23 17:35:51 -0500

    Puestas las rutas papu

--- tracktalk/src/App.js
+++ tracktalk/src/App.js
@@ -1,5 +1,11 @@
 import { Login } from './components/Login';
 import {Navbar} from './components/Navbar';
+import { About } from './components/About';
+import { Lists } from './components/Lists';
+import { Music } from './components/Music';
+import { Login } from './components/Login';
+import { Home } from './components/Login';
+
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
@@ -8,6 +14,11 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/Login" element={<Login/>}/>
+        <Route path="/SignIn" element={<SignIn/>}/>
+        <Route path="/Home" element={<Home/>}/>
+        <Route path="/Music" element={<Music/>}/>
+        <Route path="/Lists" element={<Lists/>}/>
+        <Route path="/About" element={<About/>}/>
       </Routes>
     </BrowserRouter>
   );
